fc9d0e458f6a73d16c3f3571f09f3c4f68abfd43
julian-salinas <juliansalinasm@gmail.com>
2022-05-28 19:51:24 -0300

    feat: Facebook object

---
+++ crm/applications/facebook_api/tools/requests_tools.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+def application_json():
+    return { "content-type" : "application/json" }
+
+
+def generate_message_payload(recv_id, message_content):
+    '''
+    Función que genera payload para mensajes privados
+    '''
+    send_message_data = {
+        "messaging_type": "RESPONSE",
+        "recipient": {
+            "id": recv_id,
+        },
+        "message": {
+            "text": str(message_content)
+        }
+    }
+
+    return str(send_message_data)
+
+
+def generate_reply_button_payload(comment_id, message_content):
+    data = {
+        "recipient": {
+            # "post_id": "101213069228786_106005088749584"
+            # Post id es cuando respodemos a un post de un cliente
+
+            "comment_id": str(comment_id)
+        },
+        
+        "message": {
+            "attachment":{
+                "type":"template",
+
+                # El payload podría ser recibido por parámetro
+                "payload": message_content
+            }
+        }
+    }
+
+    return str(data)
+
+
+def generate_private_reply_payload(comment_id, message_content):
+    data = {
+        "recipient": {
+            "comment_id": str(comment_id)
+        },
+        
+        "message": {
+                "text": str(message_content)
+        }
+    }
+
+    return str(data)
+    
